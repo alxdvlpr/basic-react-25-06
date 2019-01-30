a7639e5ec4802cd817e5fb9a60a39fbef2f41a21
Oleksii Vostrykov <alxdvlpr@gmail.com>
2019-01-30 22:50:39 +0100

    HomeWork 4.4: Add comment into Article

--- src/reducer/articles.js
+++ src/reducer/articles.js
@@ -1,4 +1,4 @@
-import { DELETE_ARTICLE } from '../constants'
+import { CREATE_COMMENT, DELETE_ARTICLE } from '../constants'
 import { normalizedArticles } from '../fixtures'
 
 export default (articlesState = normalizedArticles, action) => {
@@ -7,6 +7,22 @@ export default (articlesState = normalizedArticles, action) => {
   switch (type) {
     case DELETE_ARTICLE:
       return articlesState.filter((article) => article.id !== payload.id)
+
+    case CREATE_COMMENT:
+      const {
+        comment: { articleId, id }
+      } = action.payload
+      const updatedArticles = [...articlesState]
+      const parentArticleComments =
+        articlesState.find((article) => article.id === articleId).comments || []
+      const parentArticleIndex = articlesState.findIndex(
+        (article) => article.id === articleId
+      )
+
+      parentArticleComments.push(id)
+      updatedArticles[parentArticleIndex].comments = parentArticleComments
+
+      return updatedArticles
 
     default:
       return articlesState
